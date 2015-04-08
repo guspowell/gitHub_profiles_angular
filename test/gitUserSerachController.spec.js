4d61d91813a74d2c65eba985eecb9717fa0c7566
guspowell <augustuspowell@me.com>
2015-04-08 15:43:08 +0100

    added controller and mustache for search results

--- test/gitUserSerachController.spec.js
+++ test/gitUserSerachController.spec.js
@@ -16,4 +16,25 @@ describe('gitUserSearchController', function(){
     expect(scope.searchTerm).toBeUndefined();
   });
 
+  describe('when searching for a user', function() {
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
+    it("displays search results", function() {
+      expect(scope.searchResult.items).toEqual(items);
+    });
+
+  });
+
 });
